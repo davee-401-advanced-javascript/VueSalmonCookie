24d4290d18987d26b603dc64a23453ecfbf912e6
Davee Sok <daveesok@gmail.com>
2021-01-07 21:14:54 -0800

    added store object to tableList

--- components/TableList.js
+++ components/TableList.js
@@ -8,13 +8,26 @@ app.component('table-list', {
   `,
   data() {
     return {
-      stores: []
+      storesArr: []
     }
   },
   methods: {
     addToStore(store) {
-      console.log('store:', store)
-    
+      let salesArray = []
+      for (var i = 0; i < 14; i++) {
+        salesArray.push(Math.round(generateRandomNum(store.minCustomer, store.maxCustomer) * store.avgCookieSale));
+      }
+      store.hourlySalesArr = salesArray;
+      store.locationTotal = salesArray.reduce((acc, val) => {
+        return acc + val;
+      }, 0);
+      this.storesArr.push(store);
+      console.log('this.stores:', this.storesArr)
     }
   }
 })
+
+
+function generateRandomNum(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
