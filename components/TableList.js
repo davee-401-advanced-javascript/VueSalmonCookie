252f5e817622086eb6450f163c308f0746a2be33
Davee Sok <daveesok@gmail.com>
2021-01-07 20:56:51 -0800

    wired emit event

--- components/TableList.js
+++ components/TableList.js
@@ -2,7 +2,19 @@ app.component('table-list', {
   template: 
   /*html*/
   `
+  <table-form @create-store="addToStore"></table-form>
+  
   <h2> This is list </h2>
-  <table-form @create-store="addToList"><table-form>
-  `
+  `,
+  data() {
+    return {
+      stores: []
+    }
+  },
+  methods: {
+    addToStore(store) {
+      console.log('store:', store)
+    
+    }
+  }
 })
